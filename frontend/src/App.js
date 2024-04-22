bdbdc3a405592593a6eec95cdff5d1a0ad3a7d74
syd_Sakura <80500783+sakura-syd@users.noreply.github.com>
2024-04-22 13:02:47 +0800

    name & path fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Summary from './pages/Summary.js';
-import Diagram from './pages/Diagram.js';
+import Summary from "./pages/Summary.js";
+import Diagram from "./pages/Diagram.js";
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import { NavBar } from "./component.js";
 
