3ea9d5fed63f8f6ccea293ac58cf6c315927dec7
gzlzgzl <93870892+gzlzgzl@users.noreply.github.com>
2024-04-22 22:36:15 +0800

    Spark code (a) with results

---
+++ Spark (a).py
@@ -0,0 +1,54 @@
+import os
+import sys
+from pyspark import SparkContext
+
+args = sys.argv
+inp = args[1] # all input data in one file
+out = args[2] # output directory
+
+def f(line):
+    new_line = [None if item == '' else item for item in line]
+    while len(new_line)<19:
+        new_line.append(None)
+    return new_line
+
+def g(x):
+    if x is None:
+        return 0
+    try:
+        ans = int(x)
+    except ValueError:
+        ans = 0
+    return ans
+
+def add_tuples(tuple1, tuple2):
+    result = tuple()
+    
+    for num1, num2 in zip(tuple1, tuple2):
+        if type(num1)==str and num1==num2:
+            sum_result = num1
+        else:
+            sum_result = num1 + num2
+        result += (sum_result,)
+
+    return result
+
+sc = SparkContext()
+
+
+inp = sc.textFile(inp)
+inp = inp.flatMap(lambda line: line.split("\n"))
+inp = inp.map(lambda line: line.split(",")).map(lambda l: f(l))
+
+overspead_num_times_info = inp.map(lambda line: (line[0],(line[1], g(line[14]),g(line[16]),g(line[15]),g(line[12])))).reduceByKey(lambda a, b: add_tuples(a, b)).coalesce(1)
+overspead_num_times_info.saveAsTextFile(out)
+
+
+sc.stop()
+
+
+
+
+
+
+
