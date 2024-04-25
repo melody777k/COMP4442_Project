39e89eb10e65b7e157eae925a68e59f3af31a8a0
Muyuan <lmy2019@outlook.com>
2024-04-25 19:14:58 +0800

    updated

--- b_sol.py
+++ b_sol.py
@@ -7,18 +7,31 @@ grouped_data = {}
 
 for item in data:
     columns = item.strip().split(",")
-    key = columns[0]
-    values = [columns[0], columns[4], columns[7]]
-    if key in grouped_data:
-        grouped_data[key].append(values)
+    if (len(columns) > 8):
+        key = columns[0]
+        values = [columns[0], columns[4], columns[7]]
+        if columns[13] and columns[13] == '1':
+            values.append(columns[13])
+        else:
+            values.append("0")
+        if key in grouped_data:
+            grouped_data[key].append(values)
+        else:
+            grouped_data[key] = [values]
     else:
-        grouped_data[key] = [values]
+        key = columns[0]
+        values = [columns[0], columns[4], columns[7], "0"]
+        if key in grouped_data:
+            grouped_data[key].append(values)
+        else:
+            grouped_data[key] = [values]
+
 
 output_filename = "b.txt"
 
 with open(output_filename, 'w', encoding='utf-8') as output_file:
     for key, values in grouped_data.items():
-        output_file.write(f"DriverID: {key}\n")
+        # output_file.write(f"{key}\n")
         for value in values:
-            output_file.write(f"Speed: {', '.join(value)}\n")
-        output_file.write("\n")
+            output_file.write(f"{','.join(value)}\n")
+        # output_file.write("\n")
