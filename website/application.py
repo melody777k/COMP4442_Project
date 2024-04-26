73ada5a44b3969caa2bbae4225ece8359b401a2b
syd_Sakura <80500783+sakura-syd@users.noreply.github.com>
2024-04-26 19:03:04 +0800

    updates
    Frontend: diagram framework
    Backend: added mysql.connector for connecting database
    parameters changes

--- website/application.py
+++ website/application.py
@@ -1,25 +1,26 @@
 from flask import Flask, render_template, request
 import json
+import mysql.connector
 
 application = Flask(__name__, static_url_path='/static')
 #app = Flask(__name__)
 
 def db_connection():
-	# mydb = mysql.connector.connect( host = 'database-2.c9gkgua66wjp.us-east-1.rds.amazonaws.com',
-	# user = 'group9',
-	# port = '3306',
-	# database = 'group9_db',
-	# passwd = 'comp4442')
+	mydb = mysql.connector.connect( host = 'database-2.c9gkgua66wjp.us-east-1.rds.amazonaws.com',
+	user = 'group9',
+	port = '3306',
+	database = 'group9_db',
+	passwd = 'comp4442')
 
 	# insert new database
-	# cur = mydb.cursor()
+	cur = mydb.cursor()
 	# cur.execute("select * from Students")
  
 	print("successfully connect to the database")
 	
 	# return mydb
 
-def tupleToJson(t):
+def tupleToJsonA(t):
 	jsonList = []
 	jsonList.append(t[0])
 	jsonList.append(t[1][0])
@@ -30,38 +31,53 @@ def tupleToJson(t):
 
 	return jsonList
 
+# def tupleToJsonB(t):
+# 	jsonListB = []
+
+# 	return jsonListB
+
 @application.route('/')
 def home():
 	return render_template('home.html')
 
 @application.route('/behavior',methods = ['POST', 'GET'])
 def behavior():
-	jsonLists = []
+	jsonListsA = []
 	results = open("../result.txt", encoding='utf-8')
 	with results as file:
 		for line in file.readlines():
 			line = line.strip('\n') 
 			line = eval(line)
-			jsonList = tupleToJson(line)
-			jsonLists.append(jsonList)
+			jsonList = tupleToJsonA(line)
+			jsonListsA.append(jsonList)
 
 	# print(jsonLists)
 	# print(type(jsonLists))
 
-	return render_template("behavior.html", result=jsonLists)
+	return render_template("behavior.html", result=jsonListsA)
 
 @application.route('/diagram')
 def diagram():
-		# mydb = db_connection()
+	# jsonListsB = []
+	# results = open("../b.txt", encoding='utf-8')
+	# with results as file:
+	# 	for line in file.readlines():
+	# 		line = line.strip('\n') 
+	# 		line = line.split(",")
+	# 		print(line)
+	# 		jsonList = line
+	# 		jsonListsB.append(jsonList)
+	# mydb = db_connection()
 
-		# cur = mydb.cursor()
-		# cur.execute("select * from Students")
+	# cur = mydb.cursor()
+	# cur.execute("select * from Students")
 
-		# myresult = cur.fetchall()
-		# for result in myresult:
-		# 	print(result)
+	# myresult = cur.fetchall()
+	# for result in myresult:
+	# 	print(result)
 
 	return render_template("diagram.html")
+	# return render_template("diagram.html", result = jsonListsB)
 
 if __name__ == '__main__':
 	application.run(port=3000, debug=True)
